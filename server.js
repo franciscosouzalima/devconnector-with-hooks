9e74666e3a181a2c89f06c461e027996cf8479c7
Francisco Aguiar <xicoaguiar@gmail.com>
2021-05-17 17:22:33 -0300

    new: register user

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose')
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+// Body parser middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 //DB CONFIG
 const db = require('./config/keys').mongoURI
