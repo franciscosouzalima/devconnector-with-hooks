5cd313886e96847bf6cc644b96253f9b990c41d7
Francisco Aguiar <xicoaguiar@gmail.com>
2021-05-18 23:46:06 -0300

    new: profile model

--- models/Profile.js
+++ models/Profile.js
@@ -74,7 +74,7 @@ const ProfileSchema = new Schema({
         type: String,
         required: true,
       },
-      fielofstudy: {
+      fieldofstudy: {
         type: String,
         required: true,
       },
